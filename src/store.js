04c01cae1f5d2bc3360ed5a3c5f8be08f69fbe7d
Nick Wong <nichowong@deloitte.com.au>
2018-09-10 00:27:11 +1000

    Chore - Reorder store variables and functions

--- src/store.js
+++ src/store.js
@@ -6,34 +6,22 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     KanaTest: {
-      view: "levels",
-      difficulty: 1,
-      running: false,
-      correct: 0,
-      incorrect: 0,
-      averageCPM: 0,
-      currentCPM: 0,
-      gameCount: 0
+      view: "levels", // Which view to display - "levels", "input", or "stats"
+      difficulty: 1, // Defined in `kana.js`
+      running: false, // Test is running or not
+      correct: 0, // Total correct answers for current test
+      incorrect: 0, // Total incorrect answers for current test
+      currentCPM: 0, // Average CPM for last test
+      averageCPM: 0, // Average CPM for entire session
+      testCount: 0 // Total number of tests taken
     }
   },
   mutations: {
     setView: function(state, newView) {
       state.KanaTest.view = newView;
     },
-    increaseDifficulty: function(state) {
-      state.KanaTest.difficulty += 1;
-    },
-    decreaseDifficulty: function(state) {
-      state.KanaTest.difficulty -= 1;
-    },
     setDifficulty: function(state, newDifficulty) {
       state.KanaTest.difficulty = newDifficulty;
-    },
-    increaseGameCount: function(state) {
-      state.KanaTest.gameCount += 1;
-    },
-    updateAverageCPM: function(state, newAverage) {
-      state.KanaTest.averageCPM = newAverage;
     },
     startTest: function(state) {
       state.KanaTest.running = true;
@@ -50,6 +38,12 @@ export default new Vuex.Store({
     updateCurrentCPM: function(state, currentCPM) {
       state.KanaTest.currentCPM = currentCPM;
     },
+    updateAverageCPM: function(state, newAverage) {
+      state.KanaTest.averageCPM = newAverage;
+    },
+    increaseGameCount: function(state) {
+      state.KanaTest.testCount += 1;
+    },
     resetTest: function(state) {
       state.KanaTest.currentCPM = 0;
       state.KanaTest.correct = 0;
@@ -60,27 +54,8 @@ export default new Vuex.Store({
     setView: function(context, payload) {
       context.commit("setView", payload.view);
     },
-    increaseDifficulty: function(context) {
-      context.commit("increaseDifficulty");
-    },
-    decreaseDifficulty: function(context) {
-      context.commit("decreaseDifficulty");
-    },
     setDifficulty: function(context, payload) {
       context.commit("setDifficulty", payload.difficulty);
-    },
-    increaseGameCount: function(context) {
-      context.commit("increaseGameCount");
-    },
-    updateAverageCPM: function(context, payload) {
-      // Calculate the cumulative average
-      const previousAverage = context.state.KanaTest.averageCPM;
-      const previousGameCount = context.state.KanaTest.gameCount;
-      const previousSum = previousAverage * previousGameCount;
-      const newSum = previousSum + payload.newCPM;
-      const newAverage = newSum / (context.state.KanaTest.gameCount + 1);
-
-      context.commit("updateAverageCPM", newAverage);
     },
     startTest: function(context) {
       context.commit("startTest");
@@ -98,6 +73,19 @@ export default new Vuex.Store({
     },
     updateCurrentCPM: function(context, payload) {
       context.commit("updateCurrentCPM", payload.currentCPM);
+    },
+    updateAverageCPM: function(context, payload) {
+      // Calculate the cumulative average
+      const previousAverage = context.state.KanaTest.averageCPM;
+      const previousGameCount = context.state.KanaTest.testCount;
+      const previousSum = previousAverage * previousGameCount;
+      const newSum = previousSum + payload.newCPM;
+      const newAverage = newSum / (context.state.KanaTest.testCount + 1);
+
+      context.commit("updateAverageCPM", newAverage);
+    },
+    increaseGameCount: function(context) {
+      context.commit("increaseGameCount");
     },
     resetTest: function(context) {
       context.commit("resetTest");
